af118ec5fcd58387430421cd31ca57c5afe7bff4
Benjamin Francis <bhf32@cornell.edu>
2024-05-24 00:09:33 -0400

    Update to basic driver functions
    Driver functions updates

--- include/MCP3008.h
+++ include/MCP3008.h
@@ -11,6 +11,26 @@
 #include <stdlib.h>
 #include "main.h"
 
-uint16_t MCP3008_read(MCP3008_data_t *dev, uint16_t chan);
+typedef struct
+{
+
+	GPIO_TypeDef *CS_Port;
+
+	uint16_t CS_Pin;
+
+    SPI_HandleTypeDef *hspi;
+
+}MCP3008_Handle_t;
+
+typedef enum
+{
+	MCP3008_OK 						 = 0,
+	MCP3008_ERROR					 = 1
+
+}MCP3008_STATUS;
+
+MCP3008_STATUS MCP3008_handle_init(MCP3008_data_t *handle, GPIO_TypeDef *CS_Port, uint16_t CS_Pin, SPI_HandleTypeDef *hspi);
+
+uint16_t MCP3008_read(MCP3008_data_t *handle, uint16_t chan);
 
 #endif /* __MCP3008_H_ */
