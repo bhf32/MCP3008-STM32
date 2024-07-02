efc76c08e36469ce69f01e7bfc8881e8117d7632
Benjamin Francis <bhf32@cornell.edu>
2024-07-01 20:52:46 -0400

    Updated Comments
    Moved the brief and function definition comments to header file.

--- include/MCP3008.h
+++ include/MCP3008.h
@@ -11,12 +11,12 @@
 #include <stdlib.h>
 #include "stm32l5xx_hal.h"
 
-/** Bit Manipulation Macros */
+/* Bit Manipulation Macros */
 #define SHIFT_4    			    	   4
 #define SHIFT_8     				   8
 #define FIRST_10_BITS_MASK 			   0x03FF
 
-/** Limits and Commonly used conversion Macro*/
+/* Limits and Commonly used conversion Macro*/
 #define MCP3008_NUM_CHANNELS       	   8
 #define MCP3008_ADC_MAX				   1023
 #define MCP3008_SPI_TIMEOUT			   1    //1 msec timeout
@@ -45,10 +45,38 @@ typedef enum
 
 }MCP3008_STATUS;
 
+/**
+ * @brief Initializes the MCP3008 driver handle and enable the outputs
+ *
+ * @param handle - handle for MCP3008 driver
+ * @param cs_port - CS pin GPIO port
+ * @param cs_pin - CS GPIO pin number
+ * @param hspi - STM32x SPI peripheral handle
+ *
+ * @return None
+ */
 void MCP3008_handle_init(MCP3008_Handle_t *handle, GPIO_TypeDef *cs_port, uint16_t cs_pin, SPI_HandleTypeDef *hspi);
 
+
+/**
+ * @brief Reads the ADC input for a specified channel
+ *
+ * @param handle - handle for MCP3008 driver
+ * @param chan - channel number to read (0 - 7)
+ *
+ * @return the ADC input in ADC units; MCP3008_ERROR_SPI or MCP3008_ERROR_INVALID_ARG in case of error
+ */
 int16_t MCP3008_read(MCP3008_Handle_t *handle, uint8_t chan);
 
+/**
+ * @brief Reads the ADC input for a specified channel and converts to volts
+ *
+ * @param handle - handle for MCP3008 driver
+ * @param chan - channel number to read (0 - 7)
+ * @param vref - MCP3008 reference voltage (Vref)
+ *
+ * @return the ADC input in volts; MCP3008_ERROR_SPI or MCP3008_ERROR_INVALID_ARG in case of error
+ */
 float MCP3008_read_volts(MCP3008_Handle_t *handle, uint8_t chan, float vref);
 
 #endif /* __MCP3008_H_ */
--- src/MCP3008.c
+++ src/MCP3008.c
@@ -4,19 +4,8 @@
  *  Created on: May 23, 2024
  *      Author: Ben Francis
  */
-
 #include "MCP3008.h"
 
-/**
- * Initialize the MCP3008 driver handle and enable the outputs
- *
- * @param handle - handle for MCP3008 driver
- * @param cs_port - CS pin GPIO port
- * @param cs_pin - CS GPIO pin number
- * @param hspi - STM32x SPI peripheral handle
- *
- * @return None
- */
 void MCP3008_handle_init(MCP3008_Handle_t *handle, GPIO_TypeDef *cs_port, uint16_t cs_pin, SPI_HandleTypeDef *hspi){
 	//Assign the port and pins for the SPI CS pin
 	handle->CS_Port = cs_port;
@@ -25,14 +14,6 @@ void MCP3008_handle_init(MCP3008_Handle_t *handle, GPIO_TypeDef *cs_port, uint16
 	handle->hspi = hspi;
 }
 
-/**
- * Read the ADC input for a specified channel
- *
- * @param handle - handle for MCP3008 driver
- * @param chan - channel number to read (0 - 7)
- *
- * @return the ADC input in ADC units; MCP3008_ERROR_SPI or MCP3008_ERROR_INVALID_ARG in case of error
- */
 int16_t MCP3008_read(MCP3008_Handle_t *handle, uint8_t chan){
 	//Limit chan to max number of channels
 	if(chan >= MCP3008_NUM_CHANNELS){
@@ -61,15 +42,6 @@ int16_t MCP3008_read(MCP3008_Handle_t *handle, uint8_t chan){
 	return adc_reading;
 }
 
-/**
- * Read the ADC input for a specified channel and convert to volts
- *
- * @param handle - handle for MCP3008 driver
- * @param chan - channel number to read (0 - 7)
- * @param vref - MCP3008 reference voltage (Vref)
- *
- * @return the ADC input in volts; MCP3008_ERROR_SPI or MCP3008_ERROR_INVALID_ARG in case of error
- */
 float MCP3008_read_volts(MCP3008_Handle_t *handle, uint8_t chan, float vref){
 	//Verify positive voltage for vref
 	if(vref < 0){
